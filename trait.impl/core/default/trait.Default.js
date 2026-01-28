c32fa90f90e141b25db6da4ee10a278f53b5fe07
ZhiyuanSue <108735103+ZhiyuanSue@users.noreply.github.com>
2026-01-28 06:45:20 +0000

    Deploying to gh-pages from @ ZhiyuanSue/linked_list_r4l@a46af8ee6721eb32c1644cb157963cd0eb935679 🚀

---
+++ trait.impl/core/default/trait.Default.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["linked_list_r4l",[["impl&lt;G: GetLinksWrapped&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"linked_list_r4l/struct.List.html\" title=\"struct linked_list_r4l::List\">List</a>&lt;G&gt;",0],["impl&lt;T: ?<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"linked_list_r4l/struct.Links.html\" title=\"struct linked_list_r4l::Links\">Links</a>&lt;T&gt;",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[756]}
---
+++ trait.impl/core/marker/trait.Freeze.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["linked_list_r4l",[["impl&lt;T&gt; !<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Freeze.html\" title=\"trait core::marker::Freeze\">Freeze</a> for <a class=\"struct\" href=\"linked_list_r4l/struct.Links.html\" title=\"struct linked_list_r4l::Links\">Links</a>&lt;T&gt;",1,1,["linked_list_r4l::raw_list::Links"]],["impl&lt;G&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Freeze.html\" title=\"trait core::marker::Freeze\">Freeze</a> for <a class=\"struct\" href=\"linked_list_r4l/struct.List.html\" title=\"struct linked_list_r4l::List\">List</a>&lt;G&gt;",0,1,["linked_list_r4l::linked_list::List"]],["impl&lt;G&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Freeze.html\" title=\"trait core::marker::Freeze\">Freeze</a> for <a class=\"struct\" href=\"linked_list_r4l/struct.RawList.html\" title=\"struct linked_list_r4l::RawList\">RawList</a>&lt;G&gt;",0,1,["linked_list_r4l::raw_list::RawList"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[1004]}
